ed0bd8b5875ea55e2d068a0195c579c4da0e1dec
PtrMan <robertw89@googlemail.com>
2016-01-21 20:21:04 +0100

    included javascript resources

---
+++ resources/js/startup.js
@@ -0,0 +1,8 @@
+
+function loaded() {
+	entryScriptingAccessor.setEnvironmentSetupFunction("environmentSetupFunction");
+}
+
+function environmentSetupFunction() {
+	// we do nothing for now because we just do a boring test
+}
