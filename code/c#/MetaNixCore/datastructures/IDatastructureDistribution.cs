2cbf92c92e1cae725648c2d51ab85945d742cf9f
PtrMan <robertw89@googlemail.com>
2017-02-23 14:21:08 +0100

    [D] <MetaNix> : Datastructures

---
+++ code/c#/MetaNixCore/datastructures/IDatastructureDistribution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaNix.datastructures {
+    // used to select the element index over an distribution
+    // is an unification for nars-bag and stochastic selection mechanisms of arrays
+    interface IDatastructureDistribution {
+        // \param uniformDistributionValue [0..1)
+        int getIndexByUniformRandomVariable(float uniformDistributionValue, int numberOfElements);
+    }
+}
---
+++ code/c#/MetaNixCore/datastructures/StochasticBag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaNix.datastructures {
+    class StochasticBag : IDatastructureDistribution {
+        public int getIndexByUniformRandomVariable(float uniformDistributionValue, int numberOfElements) {
+            Ensure.ensureHard(propabilities.Count > 0);
+
+            float sum = 0.0f;
+
+            for (int i =0;i< propabilities.Count;i++) {
+                sum += propabilities[i];
+                Ensure.ensureHard(sum <= 1.0f && sum >= 0.0f);
+                if( sum > uniformDistributionValue  ) {
+                    return i;
+                }
+            }
+
+            return propabilities.Count-1;
+        }
+
+        public IList<float> propabilities = new List<float>(); // sum must be one
+    }
+}
